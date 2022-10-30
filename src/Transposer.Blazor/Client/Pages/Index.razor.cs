b54e2c1424263bfa4c6958158efb98bdb05c5b53
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2022-10-30 23:41:39 +0100

    remove bootstrap, fix css issues

--- src/Transposer.Blazor/Client/Pages/Index.razor.cs
+++ src/Transposer.Blazor/Client/Pages/Index.razor.cs
@@ -5,49 +5,48 @@ using Transposer.Blazor.Shared.Handlers.ChangeKey;
 using Transposer.Blazor.Shared.Handlers.KeyDetection;
 using Transposer.Blazor.Shared.Models.Enums;
 
-namespace Transposer.Blazor.Client.Pages
+namespace Transposer.Blazor.Client.Pages;
+
+public partial class Index
 {
-    public partial class Index
+    public string SongText { get; set; } = string.Empty;
+    public string Chords { get; } = "font-family: \"Courier New\", Courier, monospace !important;";
+    public Keys[] DetectedKeyOptions { get; set; } = Enum.GetValues<Keys>();
+    public Keys SelectedSourceKey { get; set; }
+    public Keys[] TargetKeyOptions { get; set; } = Enum.GetValues<Keys>();
+    public Keys SelectedTargetKey { get; set; }
+
+    [Inject]
+    public IMediator Mediator { get; set; } = default!;
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = default!;
+
+    protected override void OnInitialized()
     {
-        public string SongText { get; set; } = string.Empty;
-        public string Chords { get; } = "font-family: \"Courier New\", Courier, monospace !important;";
-        public Keys[] DetectedKeyOptions { get; set; } = Enum.GetValues<Keys>();
-        public Keys SelectedSourceKey { get; set; }
-        public Keys[] TargetKeyOptions { get; set; } = Enum.GetValues<Keys>();
-        public Keys SelectedTargetKey { get; set; }
+    }
 
-        [Inject]
-        public IMediator Mediator { get; set; }
+    protected async Task OnSongPastedAsync()
+    {
+        var response = await Mediator.Send(new KeyDetectionRequest(SongText));
 
-        [Inject]
-        public ISnackbar Snackbar { get; set; }
+        SelectedSourceKey = response.DetectedKey;
 
-        protected async override Task OnInitializedAsync()
+        if (!string.IsNullOrWhiteSpace(response.ErrorMessage))
         {
+            Snackbar.Add(response.ErrorMessage, Severity.Error);
         }
+    }
 
-        protected async Task OnSongPastedAsync()
+    private async Task TransposeSongAsync()
+    {
+        var response = await Mediator.Send(new ChangeKeyRequest(SongText, SelectedSourceKey, SelectedTargetKey));
+
+        SongText = response.TransposedSongText;
+
+        if (!string.IsNullOrWhiteSpace(response.ErrorMessage))
         {
-            var response = await Mediator.Send(new KeyDetectionRequest(SongText));
-
-            SelectedSourceKey = response.DetectedKey;
-
-            if (!string.IsNullOrWhiteSpace(response.ErrorMessage))
-            {
-                Snackbar.Add(response.ErrorMessage, Severity.Error);
-            }
-        }
-
-        private async Task TransposeSongAsync()
-        {
-            var response = await Mediator.Send(new ChangeKeyRequest(SongText, SelectedSourceKey, SelectedTargetKey));
-
-            SongText = response.TransposedSongText;
-
-            if (!string.IsNullOrWhiteSpace(response.ErrorMessage))
-            {
-                Snackbar.Add(response.ErrorMessage, Severity.Error);
-            }
+            Snackbar.Add(response.ErrorMessage, Severity.Error);
         }
     }
 }
